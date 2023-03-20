40ba7e8b243ee19693b922cc64637fb4ddcb8b0a
loyidandrews <loyidandrews@gmail.com>
2023-03-20 15:24:58 +0100

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill
 import numpy as np 
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 from src.exception import CustomException
@@ -22,18 +23,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models): #,param
+def evaluate_models(X_train, y_train,X_test,y_test,models,param): #
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]   #Going through each and every model
-            #para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)  #We can also apply Randomizer CV instead of GridsearchCV , cv -crossValidation
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train) #Train model
 
             #model.fit(X_train, y_train)  # Train model
